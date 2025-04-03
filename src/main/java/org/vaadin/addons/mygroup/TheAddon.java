c9bebced23a0fa2ed8ed16953c562411431cab78
jcgueriaud1 <jean-christophe@vaadin.com>
2025-04-03 09:59:58 +0300

    Add CSS file

--- src/main/java/org/vaadin/addons/mygroup/TheAddon.java
+++ src/main/java/org/vaadin/addons/mygroup/TheAddon.java
@@ -1,10 +1,13 @@
 package org.vaadin.addons.mygroup;
 
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
 
+@CssImport("./the-addon.css")
 public class TheAddon extends Div {
 
     public TheAddon() {
         setText("Hello");
+        addClassName("test-css");
     }
 }
